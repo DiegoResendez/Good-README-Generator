c22565dd06186f7449b644a2caf47fcac658e66c
unknown <diego.resendez@yahoo.com>
2020-08-08 17:46:18 -0700

    js update

--- index.js
+++ index.js
@@ -1,12 +1,77 @@
-const questions = [
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 
+const questions = [
+    {
+        type: "input",
+        message: "Enter a Project Title:",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter a Project Description:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Enter a Table of Contents(comma separated, lowercase):",
+        name: "tableOfContents"
+    },
+    {
+        type: "editor",
+        message: "Enter an Installation Guide:",
+        name: "installation"
+    },
+    {
+        type: "editor",
+        message: "Enter Usage Guide:",
+        name: "usage"
+    },
+    {
+        type: "checkbox",
+        message: "Check licenses used:",
+        name: "license",
+        choices: ['MIT', 'GPLv3', 'AGPL']
+    },
+    {
+        type: "input",
+        message: "Enter Contributors(github username, comma separated):",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "Enter Tests ran(comma separated):",
+        name: "tests"
+    },
+    {
+        type: "input",
+        message: "Enter your GitHub email:",
+        name: "questionsEmail"
+    },
+    {
+        type: "input",
+        message: "Enter the URL to your GitHub profile picture",
+        name: "questionsPic"
+    }
 ];
 
-function writeToFile(fileName, data) {
+function writeToFile(fileName, data) 
+{
+    fs.writeFile(fileName, data, 'utf-8', (err) => {
+        if(err) throw err;
+        console.log('Success!');
+    });
 }
 
-function init() {
-
+function init() 
+{
+    inquirer
+    .prompt(questions)
+    .then( (response) => {
+        console.log(response);
+        writeToFile("README.md", generateMarkdown(response));
+    });
 }
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,121 @@
-function generateMarkdown(data) {
-  return `
-# ${data.title}
-
+function generateMarkdown(data) 
+{
+  return `${generateTitle(data.title)}
+  ${generateDescription(data.description)}
+  ${generateTOC(data.tableOfContents)}
+  ${generateInstallation(data.installation)}
+  ${generateUsage(data.usage)}
+  ${generateLicense(data.license)}
+  ${generateContributing(data.contributing)}
+  ${generateTests(data.tests)}
+  ${generateQuestions(data.questionsEmail, data.questionsPic)}
 `;
 }
 
+function generateTitle(title) 
+{
+  return `# ${title}
+`;
+}
+
+function generateDescription(description) 
+{
+  return `## Description
+${description}
+`;
+}
+
+function generateTOC(tableOfContents) 
+{
+  let toc = '## Table Of Contents\n';
+  let contents = tableOfContents.split(',');
+
+  contents.forEach(element => {
+
+    toc += `* [${element.charAt(0).toUpperCase() + element.slice(1)}](#${element})
+`;
+  });
+  return toc;
+}
+
+function generateInstallation(installation) 
+{
+  return `## Installation
+${installation}
+`;
+}
+
+function generateUsage(usage) 
+{
+  return `## Usage
+${usage}
+`;
+}
+
+function generateLicense(license) 
+{
+  let lic = '## License\n';
+
+  license.forEach(element => 
+    {
+    if(element === 'MIT') 
+    {
+      lic += `
+* [![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+`;
+    }
+    else if (element === 'GPLv3') 
+    {
+      lic += `
+* [![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+`;
+    }
+    else if (element === 'AGPL') 
+    {
+      lic += `
+* [![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)
+`;
+    }
+  });
+
+  return lic;
+}
+
+function generateContributing(contributing) 
+{
+  let contributors = contributing.split(',');
+  let cont = "## Contributing\n";
+
+  contributors.forEach(element => {
+    cont += `
+* [${element}](http://github.com/${element})
+`;
+  });
+
+  return cont;
+}
+
+function generateTests(tests) 
+{
+  let testArr = tests.split(',');
+  let test = "## Test\n";
+
+  testArr.forEach(element => {
+    test += `
+* ${element}
+`;
+  });
+
+  return test;
+}
+
+function generateQuestions(questionsEmail, questionsPic) {
+  let questions = "## Questions";
+
+  questions += `
+![Image of Tony](${questionsPic})
+* Email: ${questionsEmail}
+`
+  return questions;
+}
 module.exports = generateMarkdown;
